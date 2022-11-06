f813abe625c3ee392711ed139a035a56da69e422
Renaldas Bendikas <renaldasb1999@gmail.com>
2022-11-06 14:43:01 +0000

    add allauth, delete current login form, connect register user

--- golf/views.py
+++ golf/views.py
@@ -1,25 +1,7 @@
 import datetime
 from django.shortcuts import render, reverse, get_object_or_404
 from django.views import generic
-# from django.http import HttpResponseRedirect
-# from django.contrib import messages
-# from django.contrib.auth.models import User
-# from django.utils.timezone import now
-# from .models import ClassName, User, Booking
-# from .forms import UserForm, BookingForm
 
-# # Checking the classes that got booked
-# total_classes = ClassName.objects.filter(booking__status='Available').count()
-# booked_places = Booking.objects.values('places').count()
-
-# # Removing the booked places with the total available seats
-# classes_remaining = total_classes - booked_places
 
 def main_page(request):
     return render(request, 'golf/index.html')
-
-def login_page(request):
-    return render(request, 'golf/login.html')
-
-def register_page(request):
-    return render(request, 'golf/register.html')
