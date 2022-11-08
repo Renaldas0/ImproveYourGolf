9b5066d3ecd28e6dd46630845ec6663ce19083ef
Renaldas Bendikas <renaldasb1999@gmail.com>
2022-11-08 14:36:06 +0000

    add classes to admin.py

--- django_golf/settings.py
+++ django_golf/settings.py
@@ -58,7 +58,7 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/app/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
---
+++ golf/urls.py
@@ -0,0 +1,14 @@
+"""Imports"""
+from django.urls import path
+from golf import views
+
+
+urlpatterns = [
+    path('', views.BookingEnquiry.as_view(), name='booking'),
+    path('manage_booking', views.ManageBooking.as_view(),
+         name='manage_booking'),
+    path('edit_booking/<booking_id>', views.EditBooking.as_view(),
+         name='edit_booking'),
+    path('delete_booking/<booking_id>',
+         views.DeleteBooking.as_view(), name='delete_booking'),
+    ]
